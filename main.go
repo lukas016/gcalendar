623679a4233fb22dbdc57d15c451fe38fd858a67
Lukas Koszegy <l.koszegy@gmail.com>
2023-11-04 12:58:14 +0100

    fix argument

--- main.go
+++ main.go
@@ -12,23 +12,21 @@ import (
 	"google.golang.org/api/option"
 )
 
-var (
-	update     bool
-	calendarID string
-)
-
 func main() {
-	flag.BoolVar(&update, "update", false, "update token")
-	flag.StringVar(&calendarID, "calendar", "", "calendar id")
-	credentialFile := flag.String("credential", "credential.json", "service-account json file")
+	calendarID := flag.String("calendar", "", "calendar id")
+	credentialFile := flag.String("credentials-file", "credentials.json", "service-account json file")
 	flag.Parse()
-	ctx := context.Background()
+
+	if *calendarID == "" {
+		log.Fatal("missing required argument calendar")
+	}
 
 	authConfigJson, err := os.ReadFile(*credentialFile)
 	if err != nil {
 		log.Fatalf("unable marshal credentials: %v", err)
 	}
 
+	ctx := context.Background()
 	srv, err := calendar.NewService(ctx, option.WithCredentialsJSON(authConfigJson))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Calendar client: %v", err)
@@ -36,21 +34,22 @@ func main() {
 
 	t := time.Now().Round(time.Hour)
 	tMax := t.Add(time.Hour)
-	events, err := srv.Events.List(calendarID).ShowDeleted(false).
+	events, err := srv.Events.List(*calendarID).ShowDeleted(false).
 		SingleEvents(true).TimeMin(t.Format(time.RFC3339)).TimeMax(tMax.Format(time.RFC3339)).MaxResults(10).OrderBy("startTime").Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
 	}
 	fmt.Println("Upcoming events:")
 	if len(events.Items) == 0 {
-		log.Fatal("No upcoming events found.")
+		fmt.Println("No upcoming events found.")
+		os.Exit(2)
 	} else {
 		for _, item := range events.Items {
 			date := item.Start.DateTime
 			if date == "" {
 				date = item.Start.Date
 			}
-			fmt.Printf("%v (%v) %v\n", date, item.Summary, item.Description)
+			fmt.Printf("%s (%s) %s\n", date, item.Summary, item.Description)
 		}
 	}
 }
